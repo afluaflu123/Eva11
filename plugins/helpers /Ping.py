66e8d0f1088a941d74aaf04692696b1dd404f2c6
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-05 12:57:22 +0530

    Update Ping.py

--- plugins/helpers /Ping.py
+++ plugins/helpers /Ping.py
@@ -12,7 +12,7 @@ async def ping(_, message):
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"<b>🏓 Lɪᴠᴇ Pɪɴɢ Iꜱ\n{time_taken_s:.3f}</b>")
+    await rm.edit(f"<b><i>🔥 Lɪᴠᴇ Pɪɴɢ Iꜱ {time_taken_s:.3f}</i></b>")
     await asyncio.sleep(7)
     await rm.delete()
     await message.delete()
