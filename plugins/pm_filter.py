451c2a106a1062084cee59fd9024613d09ca657d
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-12 13:59:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -899,7 +899,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>┏⍞ Tɪᴛɪʟᴇ : {search}\n┣❐ Asᴋᴇᴅ Bʏ : {message.from_user.mention}\n┣⎙ Fɪʟᴇs : [{total}](tg://need_update_for_some_feature)\n┗〄 Pᴏᴡᴇʀᴇᴅ Bʏ : [kᴇʀᴀʟᴀ Rᴏᴄᴋᴇʀs](https://t.me/Team_KL)</b>"
+        cap = f"<b>┏⍞ Tɪᴛɪʟᴇ : {search}\n┣❐ Asᴋᴇᴅ Bʏ : {message.from_user.mention}\n┣⎙ Fɪʟᴇs : [{total_results}](tg://need_update_for_some_feature)\n┗〄 Pᴏᴡᴇʀᴇᴅ Bʏ : [kᴇʀᴀʟᴀ Rᴏᴄᴋᴇʀs](https://t.me/Team_KL)</b>"
     if imdb and imdb.get('poster'):
         try:
             fmsg = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
