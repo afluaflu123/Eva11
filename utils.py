4799a0b1b6a23a4bbb85c4219cca9752704323b9
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-13 17:47:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ from typing import Union
 import re
 import os
 from datetime import datetime
-from typing import List, Any, Union, Optional, AsyncGenerator
+from typing import List
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
 import requests
@@ -39,8 +39,6 @@ class temp(object):
     MELCOW = {}
     U_NAME = None
     B_NAME = None
-    CURRENT=0
-    CANCEL = False
     SETTINGS = {}
 
 async def is_subscribed(bot, query):
@@ -150,40 +148,6 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
 # https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
-
-async def iter_messages(client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
-    """Iterate through a chat sequentially.
-    This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-    you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-    single call.
-    Parameters:
-        chat_id (``int`` | ``str``):
-            Unique identifier (int) or username (str) of the target chat.
-            For your personal cloud (Saved Messages) you can simply use "me" or "self".
-            For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-        limit (``int``):
-            Identifier of the last message to be returned.
-                
-        offset (``int``, *optional*):
-            Identifier of the first message to be returned.
-            Defaults to 0.
-    Returns:
-        ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-    Example:
-        .. code-block:: python
-            for message in app.iter_messages("pyrogram", 1, 15000):
-                print(message.text)
-    """
-    current = offset
-    while True:
-        new_diff = min(200, limit - current)
-        if new_diff <= 0:
-            return
-        messages = await client.get_messages(chat_id, list(range(current, current+new_diff+1)))
-        for message in messages:
-            yield message
-            current += 1
 
 async def broadcast_messages(user_id, message):
     try:
@@ -471,15 +435,6 @@ def remove_escapes(text: str) -> str:
         else:
             res += text[counter]
     return res
-
-def get_readable_time(seconds):
-    periods = [('d', 86400), ('h', 3600), ('m', 60), ('s', 1)]
-    result = ''
-    for period_name, period_seconds in periods:
-        if seconds >= period_seconds:
-            period_value, seconds = divmod(seconds, period_seconds)
-            result += f'{int(period_value)}{period_name}'
-    return result
 
 def humanbytes(size):
     if not size:
