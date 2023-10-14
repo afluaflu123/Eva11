c43e645f6996ab5526ac284f43f33a247e24b52f
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-14 16:33:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,17 +24,16 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        buttons = [[
-                InlineKeyboardButton('• Cʜᴀɴɴᴇʟ •', url='https://t.me/team_kl'),
-                InlineKeyboardButton('• Cʜᴇᴄᴋ Pᴍ •', url=f"https://t.me/{temp.U_NAME}?start=help")
-        ]]
+        buttons = [
+            [
+                InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+            ],
+            [
+                InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            ]
+            ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
+        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
